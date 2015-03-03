272fac9f99a124128b59739b392a840654da8386
asumah <takagi1992@gmail.com>
2015-03-03 14:10:13 +0000

    Adding pages

---
+++ script.js
@@ -0,0 +1,105 @@
+  function init() {
+    // code here.
+	console.log('add click event')
+
+	arr = Array.apply(null, Array(101)).map(function (_, i) {return i;});
+	arr = shuffle(arr)
+	
+	var encrypt = new JSEncrypt();	
+	var decrypt = new JSEncrypt();
+
+	encrypt.setPublicKey("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB")
+	decrypt.setPrivateKey("MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQABAoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fvxTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeHm7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAFz/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIMV7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATeaTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5AzilpsLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Ozuku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876")
+
+
+	var code = arr[4] + arr[50] + arr[76];
+	
+  	var encrypted = encrypt.encrypt(code.toString());
+  	var uncrypted = decrypt.decrypt(encrypted);
+
+
+
+	$.cookie('secret', encrypted);
+
+	var t = $.cookie('name');
+	console.log(encrypted);
+  }
+$(document).ready(function() {
+  $('#refresh').click(function() {
+  	alert( "Handler for .click() called." );
+  	init();
+  	$('#label').text('');
+  	$('#olh').empty();
+
+  });
+});
+  
+
+
+  $(document).on('click', "button.key", function() {
+    console.log($(this).text())   
+
+    clicked = $(this).text()
+
+    var old = $('#label').text()
+
+    if (old.length < 3 && (clicked in Array.apply(null, Array(10)).map(function (_, i) {return i;}))) {
+    	$('#label').text(old + clicked)
+    }
+
+    if(clicked == ".Delete") {
+    	$('#label').text(old.slice(0,-1))
+    }
+    
+
+    if(clicked == "Enter") {
+    	$("#olh").append("<li><a href='#''>" + old +"     "+ getResult(old) +"</a></li>").listview('refresh');
+
+
+    	$('#label').text('')
+
+    	
+    }
+});
+
+
+  function getResult(number)
+  {
+  	var decrypt = new JSEncrypt();
+  	decrypt.setPrivateKey("MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQABAoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fvxTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeHm7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAFz/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIMV7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATeaTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5AzilpsLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Ozuku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876")
+  	var uncrypted = decrypt.decrypt($.cookie('secret'));
+
+
+  	var place = 0
+  	var deplace = 0
+  	var secret = uncrypted;
+
+
+
+  	for (var i = 0; i < 3 ;i++) {
+  		for (var j = 0; j < 3 ; j++){
+  				if (number.charAt(i) == secret.charAt(j)) {
+  					if (i == j)
+  						place++;
+  					else deplace++;
+  				}
+  			
+  		}
+  	
+  	}
+  	console.log(secret)
+  	console.log(place)
+  	console.log(deplace)
+
+  	return ('P: ' + place + ' D: '+ deplace);
+  	
+
+  }
+
+
+
+ function shuffle (o){ //v1.0
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+}
+
