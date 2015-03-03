b9d9cd11fe0af3990eaf4948246e6cf1b64f2c27
asumah <takagi1992@gmail.com>
2015-03-03 15:06:54 +0000

    secret gen fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     // code here.
 	console.log('add click event')
 
-	arr = Array.apply(null, Array(101)).map(function (_, i) {return i;});
+	arr = Array.apply(null, Array(10)).map(function (_, i) {return i;});
 	arr = shuffle(arr)
 	
 	var encrypt = new JSEncrypt();	
@@ -12,8 +12,18 @@
 	decrypt.setPrivateKey("MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQABAoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fvxTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeHm7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAFz/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIMV7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATeaTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5AzilpsLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Ozuku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876")
 
 
-	var code = arr[4] + arr[50] + arr[76];
+	var code = arr[5]+ ''+ arr[4] +''+ arr[6];
 	
+
+	code = code.toString();
+
+
+	console.log(arr[5] + arr[4] + arr[6]);
+
+	if(code.length < 3)
+	{
+		code = '0' + code;
+	}
   	var encrypted = encrypt.encrypt(code.toString());
   	var uncrypted = decrypt.decrypt(encrypted);
 
@@ -45,7 +55,8 @@ $(document).ready(function() {
     var old = $('#label').text()
 
     if (old.length < 3 && (clicked in Array.apply(null, Array(10)).map(function (_, i) {return i;}))) {
-    	$('#label').text(old + clicked)
+    	$('#label').text(old + clicked);
+    	$(this).disabled = true;
     }
 
     if(clicked == ".Delete") {
